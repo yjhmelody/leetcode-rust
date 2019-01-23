911ad6aa4870cc484eb9794532a801bd74500ec1
yjhmelody <465402634@qq.com>
2019-01-23 20:15:45 +0800

    update: max_profit
    Signed-off-by: yjhmelody <465402634@qq.com>

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,4 @@ mod linked_list;
 mod two_sum;
 mod add_two_numbers;
 mod remove_duplicates;
+mod max_profit;
---
+++ src/max_profit.rs
@@ -0,0 +1,28 @@
+// eval the diff prices and plus all the postive
+pub fn max_profit(prices: Vec<i32>) -> i32 {
+    if prices.len() < 2 {
+        0
+    } else {
+        let mut profit = 0;
+        for i in 0 .. prices.len()-1 {
+            let diff_price = prices[i+1] - prices[i];
+            if diff_price > 0 {
+                profit += diff_price;
+            }
+        }
+
+        profit
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        assert_eq!(max_profit(vec![7,1,5,3,6,4]), 7);
+        assert_eq!(max_profit(vec![7,6,4,3,1]), 0);
+        assert_eq!(max_profit(vec![6,1,3,2,4,7]), 7);
+    }
+}
