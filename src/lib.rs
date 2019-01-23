7f921a5f9bf7a97985f613309557dd125487c894
yjhmelody <465402634@qq.com>
2019-01-23 20:56:31 +0800

    update: rotate_array
    Signed-off-by: yjhmelody <465402634@qq.com>

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,4 @@ mod two_sum;
 mod add_two_numbers;
 mod remove_duplicates;
 mod max_profit;
+mod rotate_array;
---
+++ src/rotate_array.rs
@@ -0,0 +1,47 @@
+// rotate array in-place by temp array
+pub fn rotate(nums: &mut Vec<i32>, k: i32) {
+    if nums.len() < 2 {
+        return;
+    }
+    let k = k as usize % nums.len();
+    let mid = nums.len() - k - 1;
+    let mut temp = Vec::new();
+    for i in mid+1 .. nums.len() {
+        temp.push(nums[i]);
+    }
+    for i in (0 ..= mid).rev() {
+        nums[i+k] = nums[i];
+    }
+    for i in 0 .. k {
+        nums[i] = temp[i];
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let mut nums = vec![1,2,3,4,5,6,7];
+        let k = 3;
+        rotate(&mut nums, k);
+        assert_eq!(nums, vec![5,6,7,1,2,3,4]);
+
+        let mut nums = vec![-1,-100,3,99];
+        let k = 2;
+        rotate(&mut nums, k);
+        assert_eq!(nums, vec![3,99,-1,-100]);
+
+        let mut nums = vec![1,2];
+        let k = 2;
+        rotate(&mut nums, k);
+        assert_eq!(nums, vec![1,2]);
+
+        let mut nums = vec![1,2,3];
+        let k = 2;
+        rotate(&mut nums, k);
+        assert_eq!(nums, vec![2,3,1]);
+    }
+}
