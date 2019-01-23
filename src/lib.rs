499a674040d0691089b2cc4b179de31f5a11f938
yjhmelody <465402634@qq.com>
2019-01-23 19:49:49 +0800

    update:remove_duplicates
    Signed-off-by: yjhmelody <465402634@qq.com>

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ mod linked_list;
 
 mod two_sum;
 mod add_two_numbers;
+mod remove_duplicates;
---
+++ src/remove_duplicates.rs
@@ -0,0 +1,28 @@
+pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+    if nums.len() == 0 || nums.len() == 1 {
+        return nums.len() as i32;
+    }
+    let mut count = 0;
+    for i in 0 .. nums.len() - 1 {
+        if nums[i] != nums[i+1] {
+            count += 1;
+            nums[count] = nums[i+1];
+        }
+    }
+
+    count as i32 + 1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let mut nums = vec![1,1,2];
+        assert_eq!(remove_duplicates(&mut nums), 2);
+
+        let mut nums = vec![0,0,1,1,1,2,2,3,3,4];
+        assert_eq!(remove_duplicates(&mut nums), 5);
+    }
+}
