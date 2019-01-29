aea10ad766dd4f8a6d2bfad86797fb9ea73ac947
yjhmelody <465402634@qq.com>
2019-01-29 21:38:21 +0800

    update: new solution about remove duplicates two
    Signed-off-by: yjhmelody <465402634@qq.com>

--- src/remove_duplicates_2.rs
+++ src/remove_duplicates_2.rs
@@ -33,7 +33,7 @@ pub fn remove_duplicates2(nums: &mut Vec<i32>) -> i32 {
 
     while j < nums.len() {
         if (nums[j] != nums[i]) || (nums[j] == nums[i] && nums[j] != nums[k]) {
-            k = i;
+            k += 1;
             nums[i + 1] = nums[j];
             i += 1;
         }
@@ -41,6 +41,18 @@ pub fn remove_duplicates2(nums: &mut Vec<i32>) -> i32 {
     }
 
     (i + 1) as i32
+}
+
+// use incremental nature
+pub fn remove_duplicates3(nums: &mut Vec<i32>) -> i32 {
+    let mut i = 0;
+    for j in 0..nums.len() {
+        if j < 2 || nums[j] > nums[i - 2] {
+            nums[i] = nums[j];
+            i += 1;
+        }
+    }
+    i as i32
 }
 
 #[cfg(test)]
@@ -69,5 +81,17 @@ mod tests {
 
         let mut nums = vec![1, 1, 1, 2, 2, 3];
         assert_eq!(remove_duplicates2(&mut nums), 5);
+    }
+
+    #[test]
+    fn test3() {
+        let mut nums = vec![1, 1, 1, 2, 2, 3];
+        assert_eq!(remove_duplicates3(&mut nums), 5);
+
+        let mut nums = vec![0, 0, 1, 1, 1, 1, 2, 3, 3];
+        assert_eq!(remove_duplicates3(&mut nums), 7);
+
+        let mut nums = vec![1, 1, 1, 2, 2, 3];
+        assert_eq!(remove_duplicates3(&mut nums), 5);
     }
 }
