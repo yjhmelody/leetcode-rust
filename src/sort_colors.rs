80e5400e54355bef5389051d82f88d57e98b7aaa
yjhmelody <465402634@qq.com>
2019-01-30 18:43:32 +0800

    update: new solution about sort colors
    Signed-off-by: yjhmelody <465402634@qq.com>

--- src/sort_colors.rs
+++ src/sort_colors.rs
@@ -13,8 +13,25 @@ pub fn sort_colors(nums: &mut Vec<i32>) {
         }
         base += count;
     }
+}
 
-    println!("{:?}", colors);
+// three-way quick sort
+pub fn sort_colors2(nums: &mut Vec<i32>) {
+    let mut high = nums.len();
+    let mut i = 0;
+    let mut low = 0;
+    while i < high {
+        if nums[i] == 0 {
+            nums.swap(i, low);
+            i += 1;
+            low += 1;
+        } else if nums[i] == 2 {
+            high -= 1;
+            nums.swap(i, high);
+        } else {
+            i += 1;
+        }
+    }
 }
 
 
@@ -26,6 +43,13 @@ mod tests {
     fn test1() {
         let mut nums = vec![2, 0, 2, 1, 1, 0];
         sort_colors(&mut nums);
+        assert_eq!(nums, vec![0, 0, 1, 1, 2, 2]);
+    }
+
+    #[test]
+    fn test2() {
+        let mut nums = vec![2, 0, 2, 1, 1, 0];
+        sort_colors2(&mut nums);
         assert_eq!(nums, vec![0, 0, 1, 1, 2, 2]);
     }
 }
