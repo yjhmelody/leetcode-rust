d308f12d72dcb49125a89205b86e763df3165247
yjhmelody <465402634@qq.com>
2019-02-02 21:50:04 +0800

    update: rewrite new one solution for reverse vowels
    Signed-off-by: yjhmelody <465402634@qq.com>

--- src/reverse_vowels_of_a_string.rs
+++ src/reverse_vowels_of_a_string.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code)]
 
+// double pointers
 pub fn reverse_vowels(s: String) -> String {
     if s.len() < 1 {
         return s;
@@ -23,6 +24,32 @@ pub fn reverse_vowels(s: String) -> String {
     res.iter().collect::<String>()
 }
 
+// other express method for double pointers
+pub fn reverse_vowels2(s: String) -> String {
+    if s.len() < 1 {
+        return s;
+    }
+    let mut i = 0;
+    let mut j = s.len() - 1;
+    let mut res = s.chars().collect::<Vec<char>>();
+    while i < j {
+        if !is_vowel(res[i]) {
+            i += 1;
+            continue;
+        }
+        if !is_vowel(res[j]) {
+            j -= 1;
+            continue;
+        }
+        if i < j {
+            res.swap(i, j);
+        }
+        i += 1;
+        j -= 1;
+    }
+    res.iter().collect::<String>()
+}
+
 fn is_vowel(c: char) -> bool {
     match c {
         'a' | 'A' | 'e' | 'E' | 'i' | 'I' | 'o' | 'O' | 'u' | 'U' => true,
@@ -42,5 +69,15 @@ mod tests {
         );
 
         assert_eq!(reverse_vowels(String::from(".")), String::from("."));
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(
+            reverse_vowels2(String::from("leetcode")),
+            String::from("leotcede")
+        );
+
+        assert_eq!(reverse_vowels2(String::from(".")), String::from("."));
     }
 }
