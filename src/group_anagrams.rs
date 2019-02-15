1f76f64cd342a7cd62b4c5b4b2a548cd92e09f85
yjhmelody <465402634@qq.com>
2019-02-15 20:58:58 +0800

    update: new solution for group anagrams
    Signed-off-by: yjhmelody <465402634@qq.com>

--- src/group_anagrams.rs
+++ src/group_anagrams.rs
@@ -1,19 +1,19 @@
 #![allow(dead_code)]
 
-use crate::sort_colors::sort_colors;
-
 // Sort all strings and then record their index in hash map
 pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
     use std::collections::HashMap;
 
     let mut map = HashMap::new();
     let mut res = vec![];
-    let sorted_strs = strs.iter().fold(vec![], |mut acc, s| {
-        let mut s = s.clone().into_bytes();
-        s.sort();
-        acc.push(s);
-        acc
-    });
+    let sorted_strs: Vec<Vec<u8>> = strs
+        .iter()
+        .map(|s| {
+            let mut s = s.clone().into_bytes();
+            s.sort();
+            s
+        })
+        .collect();
 
     for i in 0..strs.len() {
         let temp = sorted_strs[i].clone();
@@ -29,6 +29,25 @@ pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
     }
 
     res
+}
+
+// ref other people's method
+pub fn group_anagrams2(strs: Vec<String>) -> Vec<Vec<String>> {
+    use std::collections::HashMap;
+
+    strs.into_iter()
+        .fold(HashMap::new(), |mut map, s| {
+            map.entry(s.bytes().fold([0; 26], |mut hash, b| {
+                hash[(b - b'a') as usize] += 1u8;
+                hash
+            }))
+                .or_insert(vec![])
+                .push(s);
+            map
+        })
+        .into_iter()
+        .map(|s| s.1)
+        .collect()
 }
 
 #[cfg(test)]
@@ -48,10 +67,29 @@ mod tests {
 
         let res = group_anagrams(strs);
 
-        // random order
         assert_eq!(
             res,
             vec![vec!["eat", "tea", "ate"], vec!["tan", "nat"], vec!["bat"]]
         )
+    }
+
+    #[test]
+    fn test2() {
+        let strs = vec![
+            "eat".to_string(),
+            "tea".to_string(),
+            "tan".to_string(),
+            "ate".to_string(),
+            "nat".to_string(),
+            "bat".to_string(),
+        ];
+
+        let res = group_anagrams2(strs);
+
+        // random order
+        // assert_eq!(
+        //   res,
+        //   vec![vec!["eat", "tea", "ate"], vec!["tan", "nat"], vec!["bat"]]
+        //)
     }
 }
